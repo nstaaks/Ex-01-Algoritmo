eea2e5b1543a46fe5374a53f36bd380f631b234b
natiele staaks <natistaaks@gmail.com>
2016-03-10 22:27:59 -0300

    exercicio 1

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,9 +1,20 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-int main()
-{
-	printf("Ola Git");
+int main() {
+	int N;
+	int NA;
+	int NS;
+	printf("Digite um numero:");
+	scanf_s("%d", &N);
 
-		return 0;
+	NA = (N - 1);
+	NS = (N + 1);
+	printf("numero antecessor:%d,\nnumero digitado:%d,\nnumero sucessor:%d\n", NA, N, NS);
+	system("pause");
+	return 0;
+
+
 
 }
+
